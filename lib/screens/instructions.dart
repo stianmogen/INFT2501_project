914e4d16737e29b8b8fc4b34687f46e9b41e93fc
Stian Mogen <stianfmo@stud.ntnu.no>
2021-10-25 19:56:04 +0200

    button start game

--- lib/screens/instructions.dart
+++ lib/screens/instructions.dart
@@ -31,6 +31,27 @@ class Instructions extends StatelessWidget {
                     style: bodyStyle
                 )
             ),
+            Padding(
+              padding: const EdgeInsets.all(paddingSize),
+              //https://flutter.dev/docs/release/breaking-changes/buttons
+              child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.hovered))
+                          return Colors.blue.withOpacity(0.04);
+                        if (states.contains(MaterialState.focused) ||
+                            states.contains(MaterialState.pressed))
+                          return Colors.blue.withOpacity(0.12);
+                        return null; // Defer to the widget's default.
+                      },
+                    ),
+                  ),
+                  onPressed: () { },
+                  child: Text(AppLocalizations.of(context)!.startGame)
+              )
+            )
           ],
         ),
       ),
